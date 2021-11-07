a5ff05c92471720fab784bdee3c8806aac922392
Daniel <dmolcap@gmail.com>
2021-11-07 16:42:23 +0000

    Fix some styles

--- src/App.js
+++ src/App.js
@@ -1,45 +1,36 @@
-import React, { Component } from 'react';
-import Particles from 'react-particles-js';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import './App.css';
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
 
 const particlesCustomization = {
-   particles: {
-      line_linked: {
-         shadow: {
-            enable: true,
-            color: "#3CA9D1",
-            blur: 5
-         }
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 800,
       },
-      number: {
-         value: 40,
-         density: {
-            enable: true,
-            value_area: 800
-         }
-      }
-   }
-}
+    },
+  },
+};
 
 class App extends Component {
-   render() {
-      return (
-         <div className="App">
-            <Particles className='particles' 
-              params={particlesCustomization}
-            />
-            <Navigation />
-            <Logo />
-            <Rank />
-            <ImageLinkForm />
-            {/* <FaceRecognition /> */}
-         </div>
-      );
-   }
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles" params={particlesCustomization} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './ImageLinkForm.css';
+import React from "react";
+import "./ImageLinkForm.css";
 
 const ImageLinkForm = () => {
-   return (
-      <div className='ma4 mt0'>
-         <p className='f3'>
-            {'I will detect faces in your pictures using AI. Give it a try'}
-         </p>
-         <div className='center'>
-            <div className='form center pa4 br3 shadow-5'>
-               <input className='f4 pa2 w-70 center' type='text' />
-               <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
-            </div>
-         </div>
+  return (
+    <div className="ma4 mt0">
+      <p className="f3">
+        {"I will detect faces in your pictures using AI. Give it a try"}
+      </p>
+      <div className="center">
+        <div className="form center pa4 br3 shadow-5">
+          <input className="f4 pa2 fl w-80 center" type="text" />
+          <button className="fl w-auto grow f4 link ph3 pv2 dib white bg-light-purple">
+            Detect
+          </button>
+        </div>
       </div>
-   );
-}
+    </div>
+  );
+};
 
 export default ImageLinkForm;
