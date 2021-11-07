35bb5ee664af158c2433d42742b3274b5cd5c0c1
Daniel <dmolcap@gmail.com>
2021-11-07 19:42:16 +0000

    Add box around faces recognition

--- src/App.js
+++ src/App.js
@@ -27,8 +27,28 @@ const particlesCustomization = {
 class App extends Component {
   constructor() {
     super();
-    this.state = { input: "", imageUrl: "" };
+    this.state = { input: "", imageUrl: "", box: {} };
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({ box: box });
+  };
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
@@ -38,14 +58,10 @@ class App extends Component {
     event.preventDefault();
     console.log("submitted");
     this.setState({ imageUrl: this.state.input });
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function (response) {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      },
-      function (err) {}
-    );
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((res) => this.displayFaceBox(this.calculateFaceLocation(res)))
+      .catch((err) => console.log(err));
   };
 
   render() {
@@ -59,7 +75,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onSubmit={this.onSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
       </div>
     );
   }
