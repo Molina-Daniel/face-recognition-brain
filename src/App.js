ebf38ff1e55ab91a1d7310d4ee08fb38549d99e9
Daniel <dmolcap@gmail.com>
2022-02-08 17:39:10 +0100

    Move endpoints to Heroku server

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
   onSubmit = (event) => {
     event.preventDefault();
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3001/imageurl", {
+    fetch("https://salty-thicket-07941.herokuapp.com/imageurl", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -92,7 +92,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((res) => {
         if (res) {
-          fetch("http://localhost:3001/image", {
+          fetch("https://salty-thicket-07941.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -21,7 +21,7 @@ class Signin extends React.Component {
   onSubmitSignIn = (event) => {
     event.preventDefault();
 
-    fetch("http://localhost:3001/signin", {
+    fetch("https://salty-thicket-07941.herokuapp.com/signin", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
